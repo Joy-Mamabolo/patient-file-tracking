3e4022f2e1009197d875f07342c5a1b368edc421
Joy_M <233443059+Joy-Mamabolo@users.noreply.github.com>
2025-12-26 15:10:11 +0200

    chore: updated models to define relationships

--- src/models.py
+++ src/models.py
@@ -6,6 +6,9 @@ class department(db.Model):
     department_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     department_name = db.Column(db.String(50), nullable=False, unique=True) # e.g., Radiology, Cardiology etc.
 
+    #Relationships
+    staff_members = db.relationship('Staff', back_populates='department', uselist=False)  # Assume staff members belong to one department
+
     def __repr__(self):
         return f"<Department {self.department_name}>"
 
@@ -15,6 +18,11 @@ class Staff(db.Model):
     name = db.Column(db.String(100), nullable=False)
     department_id = db.Column(db.Integer, db.ForeignKey('departments.department_id'), nullable=False) # e.g., Radiology, Cardiology etc. Will be programmed to only allow certain values
     permissions = db.Column(db.String(100), nullable=False) # e.g., Super-user, Standard User etc. Will be programmed to only allow certain values
+
+    #Relationships
+    department = db.relationship('department', back_populates='staff_members', uselist=False) # Assume staff members belong to one department
+
+
 
     __table_args__ = (
         CheckConstraint("permissions IN ('Super-user', 'Standard User','Special User')",
@@ -28,6 +36,9 @@ class Status(db.Model):
     status_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     status_name = db.Column(db.String(50), nullable=False, unique=True) # e.g., In Transit, At Department, With Staff etc.
 
+    #Relationships
+    patient_files = db.relationship('PatientFile', back_populates='status')
+
     def __repr__(self):
         return f"<Status {self.status_name}>"
 
@@ -37,6 +48,9 @@ class PatientFile(db.Model):
     patient_name = db.Column(db.String(100), nullable=False)
     dob = db.Column(db.Date, nullable=False) # Date of Birth to help distinguish patients with similar names
     status_id = db.Column(db.Integer, db.ForeignKey('statuses.status_id'), nullable=False) # Current status of the file
+
+    #Relationships
+    status = db.relationship('Status', back_populates='patient_files')
 
     def __repr__(self):
         return f"<PatientFile {self.patient_name} - {self.dob}>"
@@ -48,6 +62,8 @@ class FileLog(db.Model):
     staff_id = db.Column(db.Integer, db.ForeignKey('staff.staff_id'), nullable=False)
     timestamp = db.Column(db.String(100), nullable=False) # Timestamp of the log entry
     status_id = db.Column(db.Integer, db.ForeignKey('statuses.status_id'), nullable=False) # Status of the file after this log entry
+
+    # Relationships to be implemented later if needed
 
     def __repr__(self):
         return f"<FileLog File No: {self.file_no} - Status: {self.status_id}>"
